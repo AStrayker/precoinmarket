7957812627423f6a0bf9a4e1b0bca645dfb4374e
AStrayker <astrajker@gmail.com>
2024-10-08 13:21:12 +0300

    Update and rename script-pc.js to script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-﻿// Пример скрипта для динамической загрузки контента
+// Пример скрипта для динамической загрузки контента
 document.querySelectorAll('#sidebar a').forEach(link => {
     link.addEventListener('click', function(event) {
         event.preventDefault();
@@ -11,3 +11,10 @@ function loadContent(category) {
     // Здесь можешь подгружать контент из других файлов (используй fetch)
     document.querySelector('#main-content').innerHTML = `<p>Загружен контент для категории: ${category}</p>`;
 }
+
+// Пример подгрузки баннера
+fetch('banner.html')
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector('#banner').innerHTML = data;
+    });
