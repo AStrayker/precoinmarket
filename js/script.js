949bdabca6fe0df64d9ec11d982e6e43a6ef2bb5
AStrayker <astrajker@gmail.com>
2024-10-15 14:41:30 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Открытие/закрытие мобильного меню
+// Скрипт для открытия/закрытия боковой панели в мобильной версии
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.sidebar').classList.toggle('open');
 });
