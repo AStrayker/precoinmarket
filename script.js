1e13fa983e3912b3da5172419559e4c1a39096ca
AStrayker <astrajker@gmail.com>
2024-10-23 14:49:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// Отключаем контекстное меню
-document.addEventListener('contextmenu', event => event.preventDefault());
-
-// Плавная прокрутка вверх
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-// Показ кнопки вверх при прокрутке вниз
-window.onscroll = function() {
-    const scrollTopBtn = document.getElementById('scrollTopBtn');
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        scrollTopBtn.style.display = "block";
-    } else {
-        scrollTopBtn.style.display = "none";
-    }
-};
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.getElementById('menu-toggle');
     const sidebar = document.getElementById('sidebar');
